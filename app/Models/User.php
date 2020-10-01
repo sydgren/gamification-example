4e9f8830dbf407a63bf9d286d761c4f30c86d365
Sydgren <henrik@henriknordquist.dk>
2020-10-01 11:50:44 +0200

    chore: install Bouncer

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Silber\Bouncer\Database\HasRolesAndAbilities;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
     use HasApiTokens;
+    use HasRolesAndAbilities;
 
     /**
      * The attributes that are mass assignable.
